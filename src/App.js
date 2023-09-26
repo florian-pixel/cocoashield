79a27c82c9a5f5726aa414a2115c6384c96e8ca7
florian-saraka-mac <florian-saraka-mac@bihar.com>
2023-09-26 13:48:10 +0000

    remove pagination

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { Amplify } from 'aws-amplify';
 
-import { withAuthenticator, Pagination } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-	  <Pagination />
-      <button onClick={signOut}>Sign out</button>
-    </>
-  );
+	return (
+		<>
+			<h1>Hello {user.username}</h1>
+			<button onClick={signOut}>Sign out</button>
+		</>
+	);
 }
 
 export default withAuthenticator(App);
