6832c6e48cf526033b1e02a67c27386d7bfb8434
florian-saraka-mac <florian-saraka-mac@bihar.com>
2023-09-27 16:00:08 +0000

    apigateaway creation

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactrestauthapicocoashield": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "cocoashieldapp95070e13": {
       "AppClientID": "string",
@@ -8,6 +15,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "cocoashieldapp8e960f80": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import { Amplify } from 'aws-amplify';
+import { Amplify, API, Auth } from 'aws-amplify';
 
-import { withAuthenticator} from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
+import { useEffect } from 'react';
 Amplify.configure(awsExports);
 
-function App({ signOut, user }) {
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
-  );
+function App() {
+	//Get API for auth
+	async function callApi() {
+		const user = await Auth.currentAuthenticatedUser()
+		const token = user.signInUserSession.idToken.jwtToken
+		console.log({ token })
+		const requestInfo = {
+			headers: {
+				Authorization: token
+			}
+		}
+		const data = await API.get('reactrestauthapicocoashield', '/users', requestInfo)
+		console.log({ data })
+	}
+
+	//Deconnexion
+	async function signOut() {
+		try {
+			await Auth.signOut({ global: true });
+		} catch (error) {
+			console.log('error signing out: ', error);
+		}
+	}
+
+	return (
+		<div>
+			<button onClick={callApi}>Call API</button>
+			<button onClick={signOut}>Déconnexion</button>
+		</div>
+	);
 }
 
 export default withAuthenticator(App);
